8bce7395d4b9a8d0e7682efee68d9cff8286bfc7
orion <orion@sourcehub.maw>
2018-08-04 19:00:18 -0500

    Finish mod_codename

--- modules/mod_codename.py
+++ modules/mod_codename.py
@@ -3,23 +3,33 @@
 # Author: Matt Weidner <matt.weidner@gmail.com>
 # Description: Slackbot cyber op codename generator
 #
-import json
-import time
+import pathlib
+import random
+
+__ADJECTIVES = []
+__NOUNS = []
 
 def get_commands():
     return [{"command": "codename", "callback": say_codename},]
 
 def say_codename(args):
-    print("[mod_codename()]", args)
-
-    return "Codename not implemented."
-
+    adj = random.choice(__ADJECTIVES)
+    noun = random.choice(__NOUNS)
+    return " ".join(["Your next cyber op will be known as OPERATION", adj.upper(), noun.upper()])
 
 def init():
+    global __ADJECTIVES, __NOUNS
     print("[DEBUG] [mod_codename.py] init() Initializing mod_codename...")
-    pass
+    adj_path = pathlib.Path("modules/codename/adjectives-clean.txt")
+    noun_path = pathlib.Path("modules/codename/nouns-clean.txt")
+    for line in adj_path.open():
+        __ADJECTIVES.append(line.strip())
+    for line in noun_path.open():
+        __NOUNS.append(line.strip())
 
 def main():
+    init()
+    say_codename({})
     pass
 
 if __name__ == "modules.mod_codename":
